f088aa153f82306caab62fe562d17837c27aa0c9
TGDT <tgductai99@gmail.com>
2026-03-13 18:03:04 +0700

    Initial commit

---
+++ src/tuhocjavacore/HelloWorld.java
@@ -0,0 +1,10 @@
+package tuhocjavacore;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Duc tai dep trai qua di!");
+		System.out.println("Duc tai dep trai qua di!");
+		System.out.println("Duc tai dep trai qua di!");
+
+	}
+}
